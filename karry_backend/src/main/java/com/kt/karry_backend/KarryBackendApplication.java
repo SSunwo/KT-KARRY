a3afd9e734914a5eebc61e46bec417406fba8386
ssunwo <aidnsunwoo@naver.com>
2025-02-27 16:31:57 +0900

    setting

---
+++ karry_backend/src/main/java/com/kt/karry_backend/KarryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kt.karry_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarryBackendApplication.class, args);
+	}
+
+}
---
+++ karry_backend/src/test/java/com/kt/karry_backend/KarryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.karry_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
