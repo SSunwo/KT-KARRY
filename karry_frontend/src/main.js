3e0a79da3ad380c062b25f0732f1d198428b01d2
ssunwo <aidnsunwoo@naver.com>
2025-03-06 16:14:12 +0900

    add login (no api)

--- karry_frontend/src/main.js
+++ karry_frontend/src/main.js
@@ -1,14 +1,15 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import store from '@/vuex/store'
+import axios from 'axios'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.config.globalProperties.$axios = axios
+app.config.globalProperties.$store = store
 
-app.mount('#app')
+app.use(router).use(store).mount('#app')
---
+++ karry_frontend/src/vuex/store.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+import getters from '@/vuex/getters'
+import mutations from '@/vuex/mutation'
+import actions from '@/vuex/actions'
+
+export default createStore({
+  state: {
+    user: null,
+    isLogin: false,
+  },
+  mutations,
+  getters,
+  actions,
+})
